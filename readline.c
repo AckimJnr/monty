a4842c97be6aa739a027a7fe8106f398feb5d7f0
AckimJnr <acklongwe@gmail.com>
2023-06-23 03:16:24 +0200

    update invalid operation

--- readline.c
+++ readline.c
@@ -10,35 +10,35 @@
 void _readline(FILE *file, stack_t **stack)
 {
 	char line[BUFFER_SIZE], *opcode;
-	int i, found = 0;
+	int i;
 	unsigned int line_number = 0;
 	instruction_t instructions[] = {
 	{"push", push},
-	{"pall", pall}
+	{"pall", pall},
+	{NULL, NULL}
 	};
 
 	while (fgets(line, sizeof(line), file) != NULL)
 	{
 		line_number++;
-		opcode = strtok(line, " \t\n");
+		opcode = strtok(line, " \n");
 
 		for (i = 0; instructions[i].opcode != NULL; i++)
 		{
 			if (strcmp(opcode, instructions[i].opcode) == 0)
 			{
 				instructions[i].f(stack, line_number);
-				found = 1;
 				break;
 			}
 		}
-
-		if (!found)
+		if (instructions[i].opcode == NULL)
 		{
-			fprintf(stderr, "L %d: unknown instruction %s", line_number,
-					instructions[i].opcode);
+			fprintf(stderr, "L%d: unknown instruction %s\n", line_number,
+					opcode);
 			fclose(file);
 			exit(EXIT_FAILURE);
 		}
+
 	}
 
 }
