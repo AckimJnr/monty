5a63aae3f6c05c12a79287762db90b42bea94d7d
AckimJnr <acklongwe@gmail.com>
2023-06-25 14:22:29 +0200

    Add verify interger function

---
+++ verify_interger.c
@@ -0,0 +1,21 @@
+#include <string.h>
+#include <ctype.h>
+/**
+ * verify_int - Verifies if the number is an interger or not
+ * @str: the string interger to be verified
+ *
+ * Return: Returns 0 if the string contains non int values otherwise return 1
+ */
+int verify_int(char str[])
+{
+	size_t i;
+
+	for (i = 0; i < strlen(str); i++)
+	{
+		if (!isdigit(str[i]))
+		{
+			return (0);
+		}
+	}
+	return (1);
+}
